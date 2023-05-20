6b80d179ae4131ba79e68fd1e7d09d6b180989ef
RA4Z <rraz639@gmail.com>
2023-05-20 10:14:12 -0300

    Adicionando Shimmer Effect no nome do usuário

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -7,6 +7,7 @@ import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
 import { CardConsultaShimmerEffect } from "../../componentes/CardConsultaShimmerEffect";
+import { InformacoesUsuarioShimmerEffect } from "../../componentes/InformacoesUsuarioShimmerEffect";
 
 export default function Principal({ navigation }) {
   const [tempo, setTempo] = useState(false);
@@ -20,11 +21,15 @@ export default function Principal({ navigation }) {
   return (
     <TelaDeFundo>
     <View style={styles.container}>
+      { tempo ?
       <InformacoesUsuario 
         nome="Olá Sônia"
         detalhes="Mais 4 consultas hoje"
         foto={soniaFoto}
       />
+      :
+      <InformacoesUsuarioShimmerEffect />
+      }
 
       <Text style={styles.texto}>Hoje</Text>
 
